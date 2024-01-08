b8c43e1d08fd7ebc619ab3cc192155abed1d93c4
rudokemper <rtakemper@gmail.com>
2024-01-08 08:59:00 -0500

    API module and middleware overhaul

---
+++ api/dataProcessing/types.ts
@@ -0,0 +1,13 @@
+export type Column = {
+  original_column: string;
+  sql_column: string;
+};
+
+export type AlertRecord = {
+  territory_name: string;
+  alert_type: string;
+  month_detec: string;
+  year_detec: string;
+  area_alert_ha: string;
+  _topic: string;
+}
--- middleware/authMiddleware.ts
+++ middleware/authMiddleware.ts
@@ -1,12 +1,6 @@
 import { Middleware } from '@nuxt/types'
-import { Auth } from '@nuxtjs/auth-next'
-import { Context } from '@nuxt/types'
 
-interface CustomContext extends Context {
-  $auth: Auth
-}
-
-const authMiddleware: Middleware = ({ app, $auth, redirect }) => {
+const authMiddleware: Middleware = ({ $auth, redirect }) => {
   const AUTH_STRATEGY = process.env.NUXT_ENV_AUTH_STRATEGY ? process.env.NUXT_ENV_AUTH_STRATEGY.replace(/['"]+/g, '') : 'none';
   if (AUTH_STRATEGY === 'auth0' && !$auth.loggedIn) {
     return redirect('/login')
