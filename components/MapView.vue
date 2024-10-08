626e351d84e236630e8bd1059add1751819cb9a2
Rudo Kemper <rtakemper@gmail.com>
2024-10-08 12:01:29 -0400

    BasemapSelector component working in Nuxt 3

---
+++ components/MapView.vue
@@ -0,0 +1,319 @@
+<template>
+  <div id="map"></div>
+  <DataFilter
+    v-if="filterColumn"
+    :data="mapData"
+    :filter-column="filterColumn"
+    :show-colored-dot="true"
+    @filter="filterValues"
+  />
+  <MapLegend
+    v-if="mapLegendContent && mapData"
+    :map-legend-content="mapLegendContent"
+    :resetLegend="resetLegend"
+    @toggle-layer-visibility="toggleLayerVisibility"
+  />
+  <BasemapSelector
+    v-if="showBasemapSelector"
+    :mapbox-style="mapboxStyle"
+    :planet-api-key="planetApiKey"
+    @basemapSelected="handleBasemapChange"
+  />
+  <!-- Add Sidebar -->
+</template>
+
+<script setup>
+import { ref, onMounted, onBeforeUnmount } from "vue";
+
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
+import { getFilePathsWithExtension } from "@/utils";
+import {
+  changeMapStyle,
+  prepareMapLegendLayers,
+  prepareCoordinatesForSelectedFeature,
+  toggleLayerVisibility as utilsToggleLayerVisibility,
+} from "@/utils/mapFunctions.ts";
+
+import DataFilter from "@/components/shared/DataFilter.vue";
+// import Sidebar from "@/components/shared/Sidebar.vue";
+import MapLegend from "@/components/shared/MapLegend.vue";
+import BasemapSelector from "@/components/shared/BasemapSelector.vue";
+
+// Define props
+const props = defineProps({
+  allowedFileExtensions: Object,
+  filterColumn: String,
+  mapLegendLayerIds: String,
+  mapboxAccessToken: String,
+  mapboxBearing: Number,
+  mapboxLatitude: Number,
+  mapboxLongitude: Number,
+  mapboxPitch: Number,
+  mapboxProjection: String,
+  mapboxStyle: String,
+  mapboxZoom: Number,
+  mapbox3d: Boolean,
+  mapData: Array,
+  mediaBasePath: String,
+  planetApiKey: String,
+});
+
+// Set up reactive state
+const map = ref(null);
+const filteredData = ref([...props.mapData]);
+const processedData = ref([...props.mapData]);
+const selectedFeature = ref(null);
+const showSidebar = ref(false);
+const showBasemapSelector = ref(false);
+const mapLegendContent = ref(null);
+const currentBasemap = ref(props.mapboxStyle);
+const resetLegend = ref(false);
+
+// Define methods
+const addDataToMap = () => {
+  // Remove existing data layers from the map
+  if (map.value) {
+    map.value.getStyle().layers.forEach((layer) => {
+      if (layer.id.startsWith("data-layer")) {
+        if (map.value.getLayer(layer.id)) {
+          map.value.removeLayer(layer.id);
+        }
+      }
+    });
+    if (map.value.getSource("data-source")) {
+      map.value.removeSource("data-source");
+    }
+  }
+
+  // Create a GeoJSON source with all the features
+  const geoJsonSource = {
+    type: "FeatureCollection",
+    features: filteredData.value.map((feature) => ({
+      type: "Feature",
+      geometry: {
+        type: feature.geotype,
+        coordinates: feature.geocoordinates,
+      },
+      properties: {
+        feature,
+      },
+    })),
+  };
+
+  // Add the source to the map
+  if (!map.value.getSource("data-source")) {
+    map.value.addSource("data-source", {
+      type: "geojson",
+      data: geoJsonSource,
+    });
+  }
+
+  // Add a layer for Point features
+  if (!map.value.getLayer("data-layer-point")) {
+    map.value.addLayer({
+      id: "data-layer-point",
+      type: "circle",
+      source: "data-source",
+      filter: ["==", "$type", "Point"],
+      paint: {
+        "circle-radius": 6,
+        "circle-color": ["get", "filter-color", ["get", "feature"]],
+        "circle-stroke-width": 2,
+        "circle-stroke-color": "#fff",
+      },
+    });
+  }
+
+  // Add a layer for LineString features
+  if (!map.value.getLayer("data-layer-linestring")) {
+    map.value.addLayer({
+      id: "data-layer-linestring",
+      type: "line",
+      source: "data-source",
+      filter: ["==", "$type", "LineString"],
+      paint: {
+        "line-color": ["get", "filter-color", ["get", "feature"]],
+        "line-width": 2,
+      },
+    });
+  }
+
+  // Add a layer for Polygon features
+  if (!map.value.getLayer("data-layer-polygon")) {
+    map.value.addLayer({
+      id: "data-layer-polygon",
+      type: "fill",
+      source: "data-source",
+      filter: ["==", "$type", "Polygon"],
+      paint: {
+        "fill-color": ["get", "filter-color", ["get", "feature"]],
+        "fill-opacity": 0.5,
+      },
+    });
+  }
+
+  // Add event listeners
+  ["data-layer-point", "data-layer-linestring", "data-layer-polygon"].forEach(
+    (layerId) => {
+      map.value.on(
+        "mouseenter",
+        layerId,
+        () => {
+          map.value.getCanvas().style.cursor = "pointer";
+        },
+        { passive: true },
+      );
+      map.value.on(
+        "mouseleave",
+        layerId,
+        () => {
+          map.value.getCanvas().style.cursor = "";
+        },
+        { passive: true },
+      );
+      map.value.on(
+        "click",
+        layerId,
+        (e) => {
+          let featureObject = JSON.parse(e.features[0].properties.feature);
+          delete featureObject["filter-color"];
+
+          // Rewrite coordinates string from [long, lat] to lat, long, removing brackets
+          if (featureObject.geocoordinates) {
+            featureObject.geocoordinates = prepareCoordinatesForSelectedFeature(
+              featureObject.geocoordinates,
+            );
+          }
+
+          selectedFeature.value = featureObject;
+          showSidebar.value = true;
+        },
+        { passive: true },
+      );
+    },
+  );
+};
+
+const filterValues = (values) => {
+  if (values.includes("null")) {
+    filteredData.value = [...processedData.value];
+  } else {
+    filteredData.value = processedData.value.filter((item) =>
+      values.includes(item[props.filterColumn]),
+    );
+  }
+  addDataToMap(); // Call this method to update the map data
+};
+
+const handleBasemapChange = (newBasemap) => {
+  changeMapStyle(map.value, newBasemap, props.planetApiKey);
+  currentBasemap.value = newBasemap;
+  map.value.once("idle", () => {
+    prepareMapCanvasContent();
+  });
+};
+
+const prepareMapCanvasContent = () => {
+  addDataToMap();
+  prepareMapLegendContent();
+};
+
+const prepareMapLegendContent = () => {
+  if (!props.mapLegendLayerIds) {
+    return;
+  }
+  map.value.once("idle", () => {
+    mapLegendContent.value = prepareMapLegendLayers(
+      map.value,
+      props.mapLegendLayerIds,
+    );
+  });
+};
+
+const toggleLayerVisibility = (item) => {
+  utilsToggleLayerVisibility(map.value, item);
+};
+
+const resetLegendVisibility = () => {
+  resetLegend.value = true;
+  // Reset the flag after the reset is handled
+  setTimeout(() => {
+    resetLegend.value = false;
+  }, 0);
+};
+
+// Lifecycle hooks
+onMounted(() => {
+  mapboxgl.accessToken = props.mapboxAccessToken;
+
+  map.value = new mapboxgl.Map({
+    container: "map",
+    style: props.mapboxStyle || "mapbox://styles/mapbox/streets-v12",
+    projection: props.mapboxProjection || "mercator",
+    center: [props.mapboxLongitude || 0, props.mapboxLatitude || -15],
+    zoom: props.mapboxZoom || 2.5,
+    pitch: props.mapboxPitch || 0,
+    bearing: props.mapboxBearing || 0,
+  });
+
+  map.value.on("load", () => {
+    if (props.mapbox3d) {
+      map.value.addSource("mapbox-dem", {
+        type: "raster-dem",
+        url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+        tileSize: 512,
+        maxzoom: 14,
+      });
+      map.value.setTerrain({ source: "mapbox-dem", exaggeration: 1.5 });
+    }
+
+    prepareMapCanvasContent();
+
+    const nav = new mapboxgl.NavigationControl();
+    map.value.addControl(nav, "top-right");
+
+    const scale = new mapboxgl.ScaleControl({
+      maxWidth: 80,
+      unit: "metric",
+    });
+    map.value.addControl(scale, "bottom-left");
+
+    const fullscreenControl = new mapboxgl.FullscreenControl();
+    map.value.addControl(fullscreenControl, "top-right");
+
+    showBasemapSelector.value = true;
+  });
+});
+
+onBeforeUnmount(() => {
+  if (map.value) {
+    map.value.remove();
+  }
+});
+</script>
+
+<style scoped>
+body {
+  margin: 0;
+  padding: 0;
+}
+
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+
+.mapboxgl-popup-content {
+  word-wrap: break-word;
+}
+
+.popup-media {
+  width: 100%;
+  display: block;
+  margin-top: 5px;
+}
+</style>
---
+++ components/shared/BasemapSelector.vue
@@ -0,0 +1,199 @@
+<template>
+  <div>
+    <div
+      class="basemap-toggle rounded shadow"
+      :class="{ active: showBasemapWindow }"
+      @click="toggleBasemapWindow"
+    >
+      <img src="/map.svg" alt="Map Icon" />
+    </div>
+    <div v-if="showBasemapWindow" class="basemap-window rounded shadow">
+      <div class="basemap-window-content">
+        <h3 class="font-semibold mb-2">{{ $t("selectBasemap") }}</h3>
+        <label>
+          <input
+            type="radio"
+            :value="{ id: 'custom', style: mapboxStyle }"
+            name="basemap"
+            v-model="selectedBasemap"
+            @change="emitBasemapChange"
+          />
+          {{ $t("yourMapboxStyleDefault") }}
+        </label>
+        <label>
+          <input
+            type="radio"
+            :value="{
+              id: 'satellite-streets',
+              style: 'mapbox://styles/mapbox/satellite-streets-v12',
+            }"
+            name="basemap"
+            v-model="selectedBasemap"
+            @change="emitBasemapChange"
+          />
+          {{ $t("mapboxSatelliteUpTo2019") }}
+        </label>
+        <label>
+          <input
+            type="radio"
+            :value="{
+              id: 'streets',
+              style: 'mapbox://styles/mapbox/streets-v12',
+            }"
+            name="basemap"
+            v-model="selectedBasemap"
+            @change="emitBasemapChange"
+          />
+          {{ $t("mapboxStreets") }}
+        </label>
+        <label v-if="planetApiKey">
+          <input
+            type="radio"
+            :value="{ id: 'planet', monthYear: monthYear }"
+            name="basemap"
+            v-model="selectedBasemap"
+            @change="emitBasemapChange"
+          />
+          {{ $t("planetMonthlyVisualBasemap") }}
+        </label>
+        <label v-if="selectedBasemap.id === 'planet'">
+          <Datepicker
+            v-model:value="monthYear"
+            format="YYYY-MM"
+            value-type="YYYY-MM"
+            type="month"
+            :default-value="maxMonth"
+            :disabled-date="setPlanetDateRange"
+            :clearable="false"
+            @selected="updatePlanetBasemap"
+          ></Datepicker>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed, defineEmits, watch } from "vue";
+
+import Datepicker from "vue-datepicker-next";
+import "vue-datepicker-next/index.css";
+
+// Define props
+const props = defineProps({
+  mapboxStyle: String,
+  planetApiKey: String,
+});
+
+// Set up reactive state
+const minMonth = "2020-09"; // The first month we have Planet NICFI monthly basemaps
+const showBasemapWindow = ref(false);
+const selectedBasemap = ref({ id: "custom", style: props.mapboxStyle });
+
+// Computed property for maxMonth
+const maxMonth = computed(() => {
+  // If the current day is less than or equal to 15, maxMonth is two months ago.
+  // Otherwise, maxMonth is the previous month.
+  // This is because Planet NICFI monthly basemaps for the previous month are published on the 15th of each month.
+  const date = new Date();
+  if (date.getDate() <= 15) {
+    date.setMonth(date.getMonth() - 2);
+  } else {
+    date.setMonth(date.getMonth() - 1);
+  }
+  const year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  month = month < 10 ? `0${month}` : month;
+  return `${year}-${month}`;
+});
+const monthYear = ref(maxMonth.value);
+
+// Emit const
+const emit = defineEmits(["basemapSelected"]);
+
+// Methods
+const toggleBasemapWindow = () => {
+  showBasemapWindow.value = !showBasemapWindow.value;
+};
+
+const emitBasemapChange = () => {
+  emit("basemapSelected", selectedBasemap.value);
+};
+
+const setPlanetDateRange = (date) => {
+  // minMonth and maxMonth are in format YYYY-MM, but date is a Date object
+  // so we need to convert it to a string in the same format
+  date = date.toISOString().slice(0, 7);
+  return date < minMonth || date > maxMonth.value;
+};
+
+const updatePlanetBasemap = () => {
+  if (selectedBasemap.value.id === "planet") {
+    selectedBasemap.value.monthYear = monthYear.value;
+    emitBasemapChange();
+  }
+};
+
+// Watchers
+watch(selectedBasemap, (newVal, oldVal) => {
+  // Update the monthYear when the planet basemap is selected
+  if (newVal.id === "planet" && newVal !== oldVal) {
+    monthYear.value = maxMonth.value;
+  }
+});
+
+watch(monthYear, (newVal, oldVal) => {
+  if (selectedBasemap.value.id === "planet") {
+    updatePlanetBasemap();
+  }
+});
+</script>
+
+<style scoped>
+.basemap-toggle {
+  position: absolute;
+  top: 147px;
+  right: 10px;
+  padding: 3px;
+  width: 30px;
+  background-color: #fff;
+  z-index: 1000;
+
+  img {
+    width: 100%;
+  }
+}
+
+.basemap-toggle:hover {
+  background-color: #f0f0f0;
+  cursor: pointer;
+}
+
+.basemap-toggle.active {
+  background-color: #fff44f;
+}
+
+.basemap-window {
+  position: absolute;
+  right: 50px;
+  top: 147px;
+  background-color: #fff;
+  border: 1px solid #ccc;
+  z-index: 1001;
+  padding: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.basemap-window-content {
+  display: flex;
+  flex-direction: column;
+}
+
+.basemap-window h3 {
+  margin-top: 0;
+}
+
+label {
+  margin-bottom: 10px;
+}
+</style>
